cb33129984c604a0bf46bc90fc9ba8e4d405d0a5
Andresflorezdev <florezandresfelipe62@gmail.com>
2026-05-01 21:58:39 -0500

    refactor(app): integrar shell del blog, rutas dinamicas y remover scaffold inicial

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => import('@/views/HomeView.vue')    // lazy loading ✅
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/post/:slug',
+      name: 'post',
+      component: () => import('@/views/PostView.vue')
     },
-  ],
+    {
+      path: '/category/:slug',
+      name: 'category',
+      component: () => import('@/views/CategoryView.vue')
+    }
+  ]
 })
 
 export default router
